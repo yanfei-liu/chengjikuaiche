08eea2df9287aa861cb87f0bf02de708ddc05bcb
Gogs <gogs@fake.local>
2020-10-20 10:31:23 +0800

    1
    1

--- app.js
+++ app.js
@@ -9,7 +9,24 @@ App({
     // 登录
     wx.login({
       success: res => {
+        var appId = 'wx9616cb5f7cfbe837';
+        var secret = '2797b46b7d86f643b6235b2a53312663';
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        // wx.request({
+        //   url: 'https://api.weixin.qq.com/sns/jscode2session?appid='
+        //   +appId+'&secret='
+        //   +secret+'&js_code='
+        //   +res.code+'&grant_type=authorization_code',
+        //   // data: {
+        //   //   code: res.code
+        //   // },
+        //   success (res){
+        //     console.log(res)
+        //   },
+        //   fail (err){
+
+        //   }
+        // })
       }
     })
     // 获取用户信息
@@ -21,7 +38,6 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,35 +4,15 @@ const app = getApp()
 
 Page({
   data: {
-
-    appId:'wx9616cb5f7cfbe837',
-    secret:'2797b46b7d86f643b6235b2a53312663',
-
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    authSetting: false,//用户是否授权
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),//用户是否授权
     driver:true,//是否是司机
   },
   // 默认加载
-  onLoad: function (){
-    var t = this;
-    // 打开后就申请授权登录
-    //查看是否授权
-    wx.getSetting({
-        success: function(res) {
-          if (res.authSetting['scope.userInfo']) {
-            // 用户授权了
-            t.setData({authSetting:res.authSetting['scope.userInfo']});
-            // 获取当前定位
-            t.selectComponent("#maps").getPosition()
-          } else {
-            //用户没有授权
-            t.setData({authSetting:false});
-          }
-        }
-      });
+  onLoad: function (e){
+    this.selectComponent("#maps").getPosition()
   },
   // 跳转至其它页面
   goToViews: function(e){
@@ -52,34 +32,5 @@ Page({
     }else{
 
     }
-  },
-  // 用户授权登陆
-  grant:function(e){
-    var t = this
-    // 获取临时code
-    wx.login({
-      success (res) {
-        if (res.code) {
-          // 获取openId等敏感信息
-          wx.request({
-            url: 'https://api.weixin.qq.com/sns/jscode2session?appid='
-            +t.data.appId+'&secret='
-            +t.data.secret+'&js_code='
-            +res.code+'&grant_type=authorization_code',
-            // data: {
-            //   code: res.code
-            // },
-            success (res){
-              console.log(res)
-            },
-            fail (err){
-
-            }
-          })
-        } else {
-          console.log('登录失败！' + res.errMsg)
-        }
-      }
-    })
-  },
+  }
 })
