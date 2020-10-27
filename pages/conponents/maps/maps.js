c8fcce81f05bbb51429d03bd5bdf6be35c66ec77
Gogs <gogs@fake.local>
2020-10-27 17:05:54 +0800

    1
    1

--- pages/conponents/maps/maps.js
+++ pages/conponents/maps/maps.js
@@ -4,7 +4,6 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
   },
 
   /**
@@ -19,98 +18,68 @@ Component({
       enableTraffic:false,
       // 显示指南针
       showCompass:true
-    }
+    },
+    // map对象
+    myMap:null
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    // getPosition(){
-    //   var t = this;
-    //   wx.getLocation({
-    //     type: 'gcj02',
-    //     success (res2) {
-    //       // 实时定位
-    //       wx.startLocationUpdateBackground({
-    //         success: (res) => {
-    //           console.log('startLocationUpdate-res', res)
-    //           const latitude = res.latitude
-    //           const longitude = res.longitude
-    //           t.setData({
-    //             latitude:latitude,
-    //             longitude:longitude,
-    //             // 标记当前位置
-    //             markers:[{
-    //               iconPath: "/pages/resources/img/user.png",
-    //               id: 0,
-    //               latitude: latitude,
-    //               longitude: longitude,
-    //               width: 50,
-    //               height: 50
-    //             }]
-    //           })
-    //         },
-    //         fail: (err) => {
-    //           console.log('startLocationUpdate-err', err)
-    //         }
-    //       })
-    //     }
-    //    })
-    // }
     getPositionAuto(){
-      let that = this;
-      that.getPosition()
-      // setInterval(function(){
-      //   that.getPosition()
-      // },15*1000);
+      // 获取定位
+      this.getPosition()
     },
     getPosition(){
       var t = this;
       // 获取所有的授权信息
       wx.getSetting({
         success(res) {
-          console.log(res)
-          // 判断手机有没有开启定位
-          wx.getSystemInfo({
-            success(r2) {
-              var isopendingwei = r2.locationEnabled;
-              if(isopendingwei==false){
-                console.log("请先开启手机GPS定位")
-              }else{
-                // 判断有没有scope.userLocationBackground授权
-                if (res.authSetting['scope.userLocationBackground']) {
-                  // 获取用户位置信息
-                  wx.getLocation({
-                    type: 'gcj02',
-                    success: (r) => {
-                      console.log('startLocationUpdate-res', r)
-                      const latitude = r.latitude
-                      const longitude = r.longitude
-                      t.setData({
-                        latitude:latitude,
-                        longitude:longitude,
-                        // 标记当前位置
-                        markers:[{
-                          iconPath: "/pages/resources/img/user.png",
-                          id: 0,
-                          latitude: latitude,
-                          longitude: longitude,
-                          width: 50,
-                          height: 50
-                        }]
-                      })
-                    },
-                    fail: (err) => {
-                      console.log('startLocationUpdate-err', err)
-                    }
-                  })
-                }else{
-                  console.log('请在设置页面授予位置权限');
-                }
+          // 判断有没有scope.userLocation授权
+          if (res.authSetting['scope.userLocation']) {
+            // 获取用户位置信息
+            wx.getLocation({
+              type: 'gcj02',
+              // 开启高精度定位
+              // isHighAccuracy:true,
+              // 高精度定位超时时间,3000ms以上才有效果
+              // highAccuracyExpireTime:5000,
+              success: (r) => {
+                console.log('startLocationUpdate-res', r)
+                const latitude = r.latitude
+                const longitude = r.longitude
+                t.setData({
+                  latitude:latitude,
+                  longitude:longitude,
+                  // 标记当前位置
+                  markers:[{
+                    iconPath: "/pages/resources/img/user.png",
+                    id: 0,
+                    latitude: latitude,
+                    longitude: longitude,
+                    width: 50,
+                    height: 50
+                  }]
+                })
+              },
+              fail: (err) => {
+                console.log('startLocationUpdate-err', err)
               }
-            }
-          })
+            })
+          }else{
+            console.log('请在设置页面授予位置权限');
+            wx.authorize({
+              scope: 'scope.userLocation',
+              success:function(res){
+                // 授权成功
+                t.getPosition();
+              },
+              fail:function(err){
+                console.log('授权失败');
+              },
+            })
+          }
         }
       })  
     }
